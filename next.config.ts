1bf06f742b33fc441a98c9f4cb82dbce211d4747
Вячеслав <tfdf63@yandex.ru>
2025-05-07 09:56:19 +0400

    upd meta

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	trailingSlash: true,
 	skipTrailingSlashRedirect: true,
-	output: 'export',
+	// output: 'export',
+	output: 'standalone',
+	distDir: 'out',
 	images: { unoptimized: true },
 
 	// Добавляем HTTP-заголовки
